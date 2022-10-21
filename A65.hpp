3d94cbc3fec4a008a42501a292e381cadf2ce16a
Gerald Kyro Villanueva Tungol <Lynxvillicana@gmail.com>
2022-10-20 18:44:49 -0700

    test

--- A65.hpp
+++ A65.hpp
@@ -4,6 +4,19 @@
 #include <iostream>
 using namespace std;
 
+void swap(int n1, int n2, int n3);
+void swaptwo(int n, int s);
+
+void swaptwo(int n, int s){
+    int temp;
+    temp = n;
+    n = s;
+    s = temp;
+}
+void swap(int n1, int n2, int n3){
+    swaptwo(n1, n2);
+    swaptwo(n1, n3);
+}
 // complete two functions
 // swap() ; to swap all three values like (a, b, c) to (c, a, b)
 //              This function will use swaptwo().
